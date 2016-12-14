5acfdd3ce95ba127f44e2919815e1a6ff895cef9
Cort Buffington <n0mjs@me.com>
2016-12-13 21:34:11 -0600

    re-organize files

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def readme():
         return file.read()
 
 setup(name='dmr',
-      version='0.10',
+      version='0.1.2',
       description='ETSI DMR (Digital Mobile Radio) Teir II Utilities',
       long_description='Modules to disassemble and assemble DMR packets, including generating and decoding varoius FEC routines',
       classifiers=[
